87053f323e774c502b8716152161863b04dc3f4b
Dominic Yeoh <yeohd100@gmail.com>
2023-02-01 14:09:45 +1100

    first commit

---
+++ Web API/Controllers/MangaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MangaController : ControllerBase
+    {
+        private static List<Web_API.Models.Mangas> Mangas_Manhwas = new ()
+        {
+            new Web_API.Models.Mangas
+            {
+                Id = 10,
+                Author = "Akira Toriyama",
+                Rating = "5/5",
+                Status = "Ongoing",
+                Title = "Dragon Ball Super"
+            }
+        };
+
+        [HttpGet] // get method to retrieve data
+        public async Task<ActionResult<List<Web_API.Models.Mangas>>> GetMangas_Manhwas()
+        {
+            return Ok(Mangas_Manhwas);
+        }
+
+        [HttpPost] // post method to send data
+        public async Task<ActionResult<List<Web_API.Models.Mangas>>> AddMangas_Manhwas(Web_API.Models.Mangas book)
+        {
+            return Ok(Mangas_Manhwas);
+        }
+    }
+}
---
+++ Web API/Models/Mangas.cs
@@ -0,0 +1,11 @@
+﻿namespace Web_API.Models
+{
+    public class Mangas // and Manhwas
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Rating { get; set; } = string.Empty;
+    }
+}
